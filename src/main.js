4e54df45a441ee83eba3f1adc7557fa909fc0792
rendfall@gmail.com <rendfall@gmail.com>
2016-10-22 20:12:45 +0200

    Print message based on input field

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-window.onload = () => {};
+window.onload = () => {
+    let $div = document.getElementById('div');
+    let $input = document.getElementById('input');
+
+    $input.addEventListener('input', function (evt) {
+        $div.innerText = this.value;
+    });
+
+    $input.addEventListener('blur', function (evt) {
+        this.focus();
+    });
+};
