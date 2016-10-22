5dcee9b8168b97c7bf25af0d9a744a31d2319ff0
rendfall@gmail.com <rendfall@gmail.com>
2016-10-22 22:31:01 +0200

    Add lib: domtastic [issue]

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,17 @@
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 
+// import domtastic from 'domtastic';
+
 export default {
     entry: 'src/main.js',
     dest: 'dist/app.js',
     format: 'es',
     sourceMap: 'inline',
+    // TODO(rendfall): ReferenceError: Element is not defined (https://github.com/webpro/DOMtastic/issues/39)
+    // globals: {
+    //     $: domtastic
+    // },
     plugins: [
         babel({
             presets: [
