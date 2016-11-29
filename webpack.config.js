245f0a413f33418e8068e3aa3984c4f7844b1e3a
rendfall <rendfall@gmail.com>
2016-11-29 21:24:19 +0100

    Bye rollup, hello webpack!

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: __dirname + '/src/main.js',
+
+    output: {
+        path: __dirname + '/dist/',
+        filename: 'app.js'
+    },
+
+    devtool: 'source-map',
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'stage-0']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: ['style', 'css', 'sass']
+            }
+        ]
+    }
+};
